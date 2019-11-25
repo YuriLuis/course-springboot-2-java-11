21194d3f1c3bcd806623cfa76317758cff6d1fff
YuriLuis <yuri.luizg@hotmail.com>
2019-11-25 18:06:46 -0300

    Payment, one-to-one association

--- src/main/java/com/yuriluis/course/config/TesteConfig.java
+++ src/main/java/com/yuriluis/course/config/TesteConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.yuriluis.course.entities.Category;
 import com.yuriluis.course.entities.Order;
 import com.yuriluis.course.entities.OrderItem;
+import com.yuriluis.course.entities.Payment;
 import com.yuriluis.course.entities.Product;
 import com.yuriluis.course.entities.User;
 import com.yuriluis.course.entities.enums.OrderStatus;
@@ -40,6 +41,7 @@ public class TesteConfig implements CommandLineRunner {
 	private OrderItemRepository orderItemRepository;
 	
 
+	@SuppressWarnings("null")
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -82,6 +84,13 @@ public class TesteConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2,oi3, oi4));
+		
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T20:53:07Z"), o1);
+		
+		o1.setPayment(pay1);  //adiciona o pagamento
+		
+		orderRepository.save(o1);
 
 	}
 	
